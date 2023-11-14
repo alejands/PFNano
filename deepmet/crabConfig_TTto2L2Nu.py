884069f8b4c3fb8bad60345917fdc1588b6f38bd
Alejandro Sanchez <alejands@andrew.cmu.edu>
2023-11-14 16:52:20 -0600

    Match template formatting

--- deepmet/crabConfig_TTto2L2Nu.py
+++ deepmet/crabConfig_TTto2L2Nu.py
@@ -3,9 +3,9 @@ from CRABClient.UserUtilities import config
 config = config()
 
 import datetime
-timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+timestamp = datetime.datetime.now().strftime('_%Y%m%d-%H%M%S')
 
-config.General.requestName = 'TTto2L2Nu_Run3Summer22_NanoAODv12_fromMiniAODv4_' + timestamp
+config.General.requestName = 'TTto2L2Nu_Run3Summer22_NanoAODv12_fromMiniAODv4' + timestamp
 config.General.workArea = 'CrabJobs'
 config.General.transferLogs = True
 config.General.instance = 'prod'
@@ -17,7 +17,7 @@ config.JobType.numCores = 4
 
 config.Data.inputDataset = '/TTto2L2Nu_TuneCP5_13p6TeV_powheg-pythia8/Run3Summer22MiniAODv4-130X_mcRun3_2022_realistic_v5-v2/MINIAODSIM'
 config.Data.splitting = 'Automatic'
-config.Data.totalUnits = 1000000
-config.Data.outLFNDirBase = '/store/user/alejands/DeepMET/' # make sure to enter *your* username
+config.Data.totalUnits = 1200000
+config.Data.outLFNDirBase = '/store/user/alejands/DeepMET/'
 
-config.Site.storageSite = 'T3_US_CMU' # site where you have permission to write
+config.Site.storageSite = 'T3_US_CMU'
